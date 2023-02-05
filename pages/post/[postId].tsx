55b8575b9c1da2d29e36f929b00d8d6ecb7db931
Phat <kimphat.devcs@gmail.com>
2023-02-05 21:52:25 +0700

    Update All Projects

---
+++ pages/post/[postId].tsx
@@ -0,0 +1,116 @@
+import { useMutation, useQuery } from "@apollo/client";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import React from "react";
+import Post from "../../components/Post";
+import { GET_ALL_POST_BY_ID } from "../../graphql/queries";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { ADD_COMMENT } from "../../graphql/mutations";
+import { toast } from "react-hot-toast";
+import Avatar from "../../components/Avatar";
+import ReactTimeago from "react-timeago";
+
+type FormData = {
+  comment: string;
+};
+
+function PostPage() {
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [addComment] = useMutation(ADD_COMMENT, {
+    refetchQueries: [GET_ALL_POST_BY_ID, "post"],
+  });
+
+  const { data } = useQuery(GET_ALL_POST_BY_ID, {
+    variables: {
+      id: router.query.postId,
+    },
+  });
+
+  const post: Post = data?.post;
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    // post comment here...
+    console.log(data);
+    const notification = toast.loading("Posting your comment....");
+    await addComment({
+      variables: {
+        post_id: router.query.postId,
+        username: session?.user?.name,
+        text: data.comment,
+        created_at: new Date(),
+      },
+    });
+    setValue("comment", "");
+    toast.success("Comment Successfully Posted!", {
+      id: notification,
+    });
+  };
+
+  return (
+    <div className="mx-auto my-7 max-w-5xl">
+      <Post post={post} />
+
+      <div className="-mt-1 rounded-b-md border border-t-0 border-gray-300 bg-white p-5 pl-16">
+        <p className="text-sm">
+          Comment as <span className="text-red-500">{session?.user?.name}</span>
+        </p>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          action=""
+          className="flex max-w-5xl flex-col space-y-2"
+        >
+          <textarea
+            {...register("comment")}
+            disabled={!session}
+            className="h-24 rounded-md border border-gray-200 p-2 pl-4 outline-none disabled:bg-gray-50"
+            placeholder={
+              session ? "What are your thoughts?" : "Please sign in to comment"
+            }
+          ></textarea>
+          <button
+            disabled={!session}
+            type="submit"
+            className="rounded-full bg-red-500 p-3 font-semibold text-white disabled:bg-gray-200"
+          >
+            Comment
+          </button>
+        </form>
+      </div>
+      <div className="-my-5 rounded-b-md border border-t-0 border-gray-300 bg-white py-5 px-10">
+        <hr className="py-2" />
+
+        {post?.comment.map((com: any) => (
+          <div
+            className="relative flex items-center sapce-x-2 space-y-5"
+            key={com?.id}
+          >
+            <hr className="absolute top-10 h-16 border left-5 z-0" />
+            <div className="z-50">
+              <Avatar seed={com.username} />
+            </div>
+            <div className="flex flex-col">
+              <p className="py-2 text-xs text-gray-400">
+                <span className="font-semibold text-gray-600">
+                  {com.username}
+                </span>{" "}
+                • <ReactTimeago date={com.created_at} />
+              </p>
+              <p>{com.text}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default PostPage;
